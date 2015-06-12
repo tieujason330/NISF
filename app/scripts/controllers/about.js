99da2f26af9637b458c61a5de10269589673c973
Jason Tieu <tieujason330@gmail.com>
2015-06-11 20:32:39 -0700

    init build

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name nisrApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the nisrApp
+ */
+angular.module('nisrApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name nisrApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the nisrApp
+ */
+angular.module('nisrApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
