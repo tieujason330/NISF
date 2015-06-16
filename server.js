35671828c01cdeff94584fc97ed5531a9650a031
Dennis Nguyen <dnguy078@ucr.edu>
2015-06-16 16:48:15 -0700

    server.js

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
-var gzippo = require('gzippo');
 var express = require('express');
+var port = process.env.PORT || 8080;
 var app = express();
- 
-app.use(express.logger('dev'));
-app.use(gzippo.staticGzip("" + __dirname + "/dist"));
-app.listen(process.env.PORT || 5000);
+
+app.use(express.static(__dirname + '/dist'));
+
+// allows the browser to GET the bower files
+app.use('/bower_components', express.static(__dirname + '/bower_components'));
+
+app.listen(port);
